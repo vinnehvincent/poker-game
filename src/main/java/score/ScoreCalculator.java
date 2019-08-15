3096892f7a995414dc35c204bef9a102f317beba
Vincent Tshabalala <vincent.b.tshabalala@gmail.com>
2019-08-15 23:18:41 +0200

    Added method to detemine four of a kind

--- src/main/java/score/ScoreCalculator.java
+++ src/main/java/score/ScoreCalculator.java
@@ -1,37 +1,63 @@
 package score;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import entities.Card;
 import entities.Score;
 import entities.Suit;
 
 public class ScoreCalculator {
-	
+
 	public Score determineHandScore(List<Card> hand) {
-		if(isFlushHand(hand))
+		if (isStraightFlushHand(hand))
+			return Score.STRAIGHT_FLUSH;
+		if (isfourOfAKindHand(hand))
+			return Score.FOUR_OF_A_KIND;
+		else if (isFlushHand(hand))
 			return Score.FLUSH;
-		else if(isStraightHand(hand))
+		else if (isStraightHand(hand))
 			return Score.STRAIGHT;
 		return Score.HIGH_CARD;
 	}
-	
+
 	private boolean isFlushHand(final List<Card> hand) {
 		Suit currentSuit = hand.get(0).getSuit();
-		return hand.stream()
-					.allMatch(c-> c.getSuit() == currentSuit);
+		return hand.stream().allMatch(c -> c.getSuit() == currentSuit);
 	}
 
 	public boolean isStraightHand(final List<Card> hand) {
-		return false;
+
+		List<Card> sortedHand = sortHand(hand);
+
+		for (int i = 1; i < 5; i++) {
+
+			int smallerCardValue = sortedHand.get(i - 1).getRank().value();
+			int largerCardValue = sortedHand.get(i).getRank().value();
+
+			if (largerCardValue - smallerCardValue != 1) {
+				return false;
+			}
+		}
+		return true;
 	}
 
-	private Score isStraightFlushHand(List<Card> hand) {
-		return Score.STRAIGHT_FLUSH;
+	private List<Card> sortHand(final List<Card> hand) {
+		return hand.stream().sorted(Card.byRank).collect(Collectors.toList());
 	}
 
-	private Score isfourOfAKindHand(List<Card> hand) {
-		return Score.FOUR_OF_A_KIND;
+	private boolean isStraightFlushHand(final List<Card> hand) {
+
+		return (isFlushHand(hand) && isStraightHand(hand));
+
+	}
+
+	private boolean isfourOfAKindHand(final List<Card> hand) {
+		
+		List<Card> sortedHand = sortHand(hand);
+		int firstCardValue = sortedHand.get(0).getRank().value();
+		
+		return hand.stream().filter(card -> card.getRank().value() == firstCardValue).count() == 4;
 	}
 
 }
