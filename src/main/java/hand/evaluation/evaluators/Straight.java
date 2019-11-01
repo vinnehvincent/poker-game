8cb3fab990cfa6058416613be170087a9763fba5
Vincent Tshabalala <vincent.b.tshabalala@gmail.com>
2019-11-01 23:26:53 +0200

    Refactored: Straight evalutor

--- src/main/java/hand/evaluation/evaluators/Straight.java
+++ src/main/java/hand/evaluation/evaluators/Straight.java
@@ -22,47 +22,32 @@ public class Straight implements HandEvaluator {
     }
 
     public static boolean hasStraight(final List<Card> hand) {
-        List<Integer> lowAceStraight = hand.stream()
+        List<Integer> ranks = hand.stream()
                                 .map(Card::getRank)
                                 .map(Rank::value)
                                 .collect(Collectors.toList());
-        lowAceStraight.sort(Comparator.naturalOrder());
-        if(lowAceStraight.contains(1)){
-            List<Integer> highAceStraight = hand.stream()
+        ranks.sort(Comparator.naturalOrder());
+        if(ranks.contains(1)){
+            List<Integer> highAceRanks = hand.stream()
                     .map(Card::getRank)
                     .map(Rank::value)
                     .collect(Collectors.toList());
-            highAceStraight.remove(highAceStraight.indexOf(1));
-            highAceStraight.add(14);
-            highAceStraight.sort(Comparator.naturalOrder());
-            return compareToStraight(lowAceStraight) || compareToStraight(highAceStraight);
+            highAceRanks.remove(highAceRanks.indexOf(1));
+            highAceRanks.add(14);
+            highAceRanks.sort(Comparator.naturalOrder());
+            return isStraight(ranks) || isStraight(highAceRanks);
         }
 
 
-        return compareToStraight(lowAceStraight);
+        return isStraight(ranks);
     }
 
-    private static boolean compareToStraight(final List<Integer> ranks) {
-        List<Integer> straightList = new ArrayList<>();
+    private static boolean isStraight(final List<Integer> ranks) {
+        List<Integer> straight = new ArrayList<>();
         for(int i = ranks.get(0); i < ranks.get(0) + ranks.size(); i++ ){
-            straightList.add(i);
+            straight.add(i);
         }
-        return ranks.equals(straightList);
+        return ranks.equals(straight);
     }
 
-    private static boolean compareAdjacent(List<Card> sortedHand) {
-        for (int i = 1; i < 5; i++) {
-            int smallerCardValue = sortedHand.get(i - 1).getRank().value();
-            int largerCardValue = sortedHand.get(i).getRank().value();
-
-            if (largerCardValue - smallerCardValue != 1) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static List<Card> sortHand(final List<Card> hand) {
-        return hand.stream().sorted(Card.byRank).collect(Collectors.toList());
-    }
 }
