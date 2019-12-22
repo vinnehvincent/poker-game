408eaff72468368abdacb895852d47358e4a5d46
Vincent Tshabalala <vincent.b.tshabalala@gmail.com>
2019-12-22 09:20:51 +0200

    Changed: game play

--- src/main/java/game/play/Poker.java
+++ src/main/java/game/play/Poker.java
@@ -1,5 +1,6 @@
 package game.play;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,7 +18,7 @@ public class Poker {
 				new Card(Suit.CLUBS,Rank.ACE),new Card(Suit.CLUBS,Rank.TWO),new Card(Suit.CLUBS,Rank.THREE), new Card(Suit.CLUBS,Rank.FOUR),
 				new Card(Suit.HEARTS,Rank.FIVE),new Card(Suit.CLUBS,Rank.SIX),new Card(Suit.SPADES,Rank.SEVEN),new Card(Suit.CLUBS,Rank.EIGHT),
 				new Card(Suit.CLUBS,Rank.NINE),new Card(Suit.DIAMOND,Rank.TEN),new Card(Suit.CLUBS,Rank.JACK),new Card(Suit.CLUBS,Rank.QUEEN),new Card(Suit.CLUBS,Rank.KING));
-		shuffle(deck);
+		deck = new ArrayList<>(shuffle(deck));
 		
 		List<Card> hand = deck.subList(0, 5);
 		Player player = new Player("Vincent",hand);
@@ -26,14 +27,14 @@ public class Poker {
 
 	private static void printPlayerHand(Player player) {
 		System.out.print("Your hand: ");
-		for (Card card : player.hand) {
-			System.out.print(card + " ");
-		}
+		player.hand.stream().forEach(card -> System.out.print(card + " "));
 		System.out.println();
 		System.out.println("You have: " + player.strength.toString().toLowerCase());
 	}
 
-	private static void shuffle(List<Card> deck) {
-		Collections.shuffle(deck);	
+	private static List<Card> shuffle(final List<Card> deck) {
+		List<Card> shuffleDeck = new ArrayList<>(deck);
+		Collections.shuffle(shuffleDeck);
+		return shuffleDeck;
 	}
 }
